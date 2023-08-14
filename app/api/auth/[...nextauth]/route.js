425ea0f8e3ca0c7b10088270bcc3198aa8b706fa
dnyanesh75 <dnyaneshlohar75@gmail.com>
2023-08-14 16:16:55 +0530

    authentication-5

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,13 +1,24 @@
-import NextAuth from 'next-auth/next'
+import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
 
-const AuthOptions =  NextAuth({
-    providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET
-        })
-    ],
-})
+const AuthOptions = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  session: {
+    generateSessionToken: () => {
+      return randomUUID?.() ?? randomBytes(32).toString("hex");
+    },
+  },
+  secret: process.env.NEXT_AUTH_SECRET
+  ,
+  pages: {
+    signIn: "/",
+    error: "/api/auth/error",
+  },
+});
 
-export {AuthOptions as GET, AuthOptions as POST}
+export { AuthOptions as GET, AuthOptions as POST };
