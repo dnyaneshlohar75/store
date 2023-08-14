0d17eccb3b5db51fed3a6388c3d1409e37eccc1b
dnyanesh75 <dnyaneshlohar75@gmail.com>
2023-08-14 15:33:52 +0530

    authentication-5

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -4,8 +4,8 @@ import GoogleProvider from "next-auth/providers/google";
 const AuthOptions =  NextAuth({
     providers: [
         GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET
+            clientId: process.env.GOOGLE_CLIENT_ID || '350775443637-mvh1ftie8qqkaabnqiqqnvtkgmg545pv.apps.googleusercontent.com',
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET || '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'
         })
     ],
 })
